ed354709084f2e1e07564b4dbe2ca542098d72e9
blazeencryption <112235601+blazeencryption@users.noreply.github.com>
2022-09-22 22:20:19 +0300

    Update restaurant.js

--- restaurant.js
+++ restaurant.js
@@ -1,8 +1,10 @@
+
 var images = [
     "Images/main-background2.jpg",
     "Images/main-background4.jpg",
     "Images/main-background6.jpg"
 ]
+var opened = 0
 const thing = document.getElementById('main-page');
 var i = 0;
 setInterval(function() {
@@ -19,3 +21,24 @@ $(".carousel").swipe({
   },
   allowPageScroll: "vertical" 
 });
+function description(element, elements) {
+  document.getElementById(element).classList.toggle('opened-description');
+  texts = document.getElementsByClassName(elements);
+  Array.from(texts).forEach(text_element => {
+    text_element.classList.toggle('opened-text')
+  });
+}
+
+function revealbar() {
+  document.getElementById('check-out-bar').classList.toggle('opened-check-out-bar');
+}
+function addtocart(element, price) {
+  document.getElementById(element).style.display="block";
+  product = [
+    {
+    name:element,
+    product_price:price
+    }
+  ]
+  console.log(product)
+}
